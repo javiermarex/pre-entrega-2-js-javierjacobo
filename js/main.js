509d8829fbbe6bbb933367b94b42b9b1ea794ad3
Javier Jacobo <javier@marex.com.ar>
2024-09-05 10:33:41 -0300

    pre-entrega 2 JS Javier Jacobo.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// Variables globales
+// Variables globales e mis productos.
 let productos = [
     { nombre: "Teclado Nisuta NS-KB8812B", precio: 2500 },
     { nombre: "Mouse Nisuta NS-MO8813", precio: 1200 },
@@ -11,20 +11,24 @@ let productos = [
     { nombre: "Auriculares Logitech H340", precio: 12000 }
 ];
 
+// Inicia mi simulador. Comienza a pedirle datos al usuario.
 function iniciarSimulador() {
     saludar();
     pedirDatos();
 }
 
+// Funcion de saludar.
 function saludar() {
     alert("Bienvenid@ a nuestra Tienda de Insumos Informáticos");
 }
 
+// Funcion para pedirle datos al visitante.
 function pedirDatos() {
     let nombre = prompt("Ingresar Nombre");
     let apellido = prompt("Ingresar Apellido");
     let anoNacimiento = parseInt(prompt("Ingresar Año de Nacimiento"));
 
+    // Corrobora que el visitante sea mayor de edad (18 años)
     if (anoNacimiento >= 2006 || anoNacimiento <= 1900) {
         alert("No se permiten datos erróneos o menores de edad");
         window.close();
@@ -35,10 +39,12 @@ function pedirDatos() {
     mostrarProductos();
 }
 
+// Funcion de saludar una vez que corrobora los datos del visitante.
 function saludarCliente(nombre, apellido) {
     alert("Hola " + nombre + " " + apellido);
 }
 
+// Funcion para mostrar las marcas que trabajamos.
 function mostrarProductos() {
     let opcion = prompt("Elija qué lista quiere ver: \n 1-Nisuta \n 2-Genius \n 3-Logitech \n 4-Salir");
 
@@ -63,6 +69,7 @@ function mostrarProductos() {
     }
 }
 
+// Funcion para mostrar los productos filtrando con map.
 function mostrarListaPorMarca(marca) {
     let productosMarca = productos.filter(p => p.nombre.includes(marca));
     let lista = productosMarca.map(p => `${p.nombre}: $${p.precio}`).join('\n');
